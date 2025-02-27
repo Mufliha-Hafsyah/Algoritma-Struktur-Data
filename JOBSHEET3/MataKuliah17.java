5ce8aaf3e2d45a55d75215270fc6e9309a473eb9
Mufliha-Hafsyah <muflihahafsyah@gmail.com>
2025-02-27 21:37:01 +0700

    add constructor

--- JOBSHEET3/MataKuliah17.java
+++ JOBSHEET3/MataKuliah17.java
@@ -9,14 +9,14 @@ public class MataKuliah17 {
     public int sks;
     public int jumlahJam;
 
+    public MataKuliah17(){
+
+    }
     public MataKuliah17(String kode, String nama, int sks, int jumlahJam){
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
-    }
-    public MataKuliah17(){
-
     }
     void tambahData (){
         String dummy;
